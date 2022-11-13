b86a8b6405297bf1a4f08db8cd6766e3ef6ba1bb
flaviusgabriel <62425734+flaviusgabriel@users.noreply.github.com>
2022-11-13 16:01:25 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -1,61 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./App.scss";
-import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
-import Modal from "react-bootstrap/Modal";
+import Login from "./components/Login";
+import ToDoPAge from "./components/ToDoPage";
 
 function App() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
   return (
-    <div className="App">
-      <h1>TODO List, made with redux</h1>
-      <button type="button" class="btn btn-primary">
-        Primary
-      </button>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="name@example.com"
-                autoFocus
-              />
-            </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Example textarea</Form.Label>
-              <Form.Control as="textarea" rows={3} />
-            </Form.Group>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
-      </Modal>
-      <div className="absolute">
-        <span>Here should be some text...</span>
-        <h1 className="absolute-vodka">Just a small title</h1>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/todo" element={<ToDoPAge />} />
+    </Routes>
   );
 }
 
